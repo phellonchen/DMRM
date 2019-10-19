1371162f75c8bd5c1504b58654bae567d5c250de
paper-coder <56750221+paper-coder@users.noreply.github.com>
2019-10-19 16:25:28 +0800

    Add files via upload

--- misc/utils.py
+++ misc/utils.py
@@ -218,3 +218,18 @@ def get_self_critical_reward(netG, netW, sample_ans_input, ques_hidden, gen_resu
     rewards = np.repeat(scores[:, np.newaxis], gen_result.shape[1], 1)
 
     return rewards
+
+class LayerNorm(nn.Module):
+    """
+    Layer Normalization
+    """
+    def __init__(self, features, eps=1e-6):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(features))
+        self.beta = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
